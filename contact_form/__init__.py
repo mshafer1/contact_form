978659a1c266fa2ec0a9c75a3f11033c6aa99fcd
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2025-01-26 16:40:52 -0600

    completelely disable csrf

--- contact_form/__init__.py
+++ contact_form/__init__.py
@@ -21,6 +21,7 @@ if _sendgrid_api_key and not _sendgrid_sender_address:
 
 app = flask.Flask(__name__)
 app.secret_key = _secret_key
+app.config["WTF_CSRF_ENABLED"] = False
 csrf = wtforms.csrf.core.CSRF()
 cors = flask_cors.CORS(app)
 
